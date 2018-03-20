73311d478b8c2df0104a8cad25e6644200376f9f
JamesFraserLogan <36831161+JamesFraserLogan@users.noreply.github.com>
2018-03-20 11:55:51 -0600

    Add files via upload

--- local.h
+++ local.h
@@ -2,7 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-typedef struct node
+volatile size_t node_depth=0; // RootNode->node[a1]->node->[a2]->....->node[an] is the topology this counter follows. A node depth of 0 denotes the root node.
+size_t max_node_depth=0; // Be careful this is set correctly, it's used to determine how much memory is alloacated to the node_indices array. This would be n in the above example.
+volatile size_t screen_count=0; // This is "i" in: RootNode->node[a1]->...->node[an]->screen[i] ; that is, this is which screen you are on in a particular node.
+volatile size_t line_count=0; // This is "j" in: RootNode->node[a1]->...->node[an]->screen[i]->line[j] ; that is, this is which line you are on in a particular screen in a particular node.
+volatile size_t *node_indices; // This is the array of node index coefficients {a1,a2,a3,...,an} in the above example.
+volatile size_t opt_counter=0; // This is which option you are on in any particular node. Registered functions can be attached to options.
+typedef struct node // Links to i other nodes, j screens, and k functional options. 
 {
 	struct screen **screen;
 	struct node **node;
@@ -10,15 +16,16 @@ typedef struct node
 	size_t nnodes;
 	size_t nopts;
 }node;
-typedef struct screen
+typedef struct screen // Each screen contains n lines of (possibly) variable length.
 {
 	char **line;
 	size_t n;
 }screen;
-struct node **bundle_node(size_t n,struct node *node,...);
-struct screen **bundle_screen(size_t n,struct screen *screen,...);
-struct node *makenode(struct screen **screen,struct node **node,size_t nscreens,size_t nnodes,size_t nopts);
-struct screen *makescreen(size_t n,char *line,...);
+struct node **bundle_node(size_t n,struct node *node,...); // Bundles n nodes into a single **node. Returns NULL on error.
+struct screen **bundle_screen(size_t n,struct screen *screen,...); //Bundles n screens into a single **screen. Returns Null on error.
+struct node *makenode(struct screen **screen,struct node **node,size_t nscreens,size_t nnodes,size_t nopts); // Returns a *node. Feel free to use NULL as an input to any **node, but do not use NULL as an input to **screen. Each node must have at least one screen. Returns NULL on error.
+struct screen *makescreen(size_t n,char *line,...); // Returns a *screen. Empty screens are not allowed. Returns NULL on error.
+void node_depth_up(struct node **node,size_t index); // Sequential function that increases node depth. Only use this starting from the root node and building up to n node depth. Use "&mynode" as input, not a bundle, as C funcitons pass by value thus a **node input allows this function to modify the target *node. Simple function with no error checking, BE CAREFUL!
 struct node **bundle_node(size_t n,struct node *node,...)
 {
 	if(n<=0||node==NULL)
@@ -131,4 +138,8 @@ struct screen *makescreen(size_t n,char *line,...)
 	ret->line=arr;
 	ret->n=n;
 	return ret;
+}
+void node_depth_up(struct node **node,size_t index)
+{
+	*node=(*node)->node[index];
 }
