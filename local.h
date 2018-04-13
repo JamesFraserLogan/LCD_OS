6b126e81a33f1306f06eb5cb36bf734138f59907
JamesFraserLogan <36831161+JamesFraserLogan@users.noreply.github.com>
2018-04-13 02:13:42 -0600

    Add files via upload

--- local.h
+++ local.h
@@ -10,27 +10,27 @@ volatile size_t *node_indices; // This is the array of node index coefficients {
 volatile size_t opt_counter=0; // This is which option you are on in any particular node. Registered functions can be attached to options.
 typedef struct node // Links to i other nodes, j screens, and k functional options. Functional options can invoke a function via the node registry, or link to another node and/or screen.
 {
-	struct node **node;
-	struct node_registry *node_registry;
-	struct screen **screen;	
-	size_t nnodes;
-	size_t nopts;
-	size_t nscreens;
+  struct node **node;
+  struct node_registry *node_registry;
+  struct screen **screen; 
+  size_t nnodes;
+  size_t nopts;
+  size_t nscreens;
 }node;
 typedef struct node_registry // Links to n registered functions to be invoked in a specific node. The order array is 0 for node navigation, and 1 for function invocation, thus functions must be properly ordered.
 {
-	size_t n;
-	char *opt_order;
-	struct registered_function **registered_funciton;
+  size_t n;
+  char *opt_order;
+  struct registered_function **registered_funciton;
 }node_registry;
 typedef struct registered_function // Prepares void functions with void arguments to be inserted into a node registry.
 {
-	void (*function)(void);
+  void (*function)(void);
 }registered_function;
 typedef struct screen // Each screen contains n lines of (possibly) variable length.
 {
-	char **line;
-	size_t n;
+  char **line;
+  size_t n;
 }screen;
 struct node **bundle_node(size_t n,struct node *node,...); // Bundles n nodes into a single **node. Returns NULL on error.
 struct registered_function **bundle_registered_functions(size_t n,struct registered_function *function,...); // Bundles n registered functions into a single **registered_function. Returns NULL on error.
@@ -47,253 +47,275 @@ void testk(void);
 void teestuk(void);
 struct node **bundle_node(size_t n,struct node *node,...)
 {
-	if(n<=0||node==NULL)
-	{
-		return NULL;
-	}
-	struct node **ret=(struct node **)malloc(n*sizeof(struct node *));
-	if(ret==NULL)
-	{
-		return NULL;
-	}
-	*ret=node;
-	va_list ap;
-	va_start(ap,node);
-	for(size_t i=1;i<n;i++)
-	{
-		*(ret+i)=va_arg(ap,struct node *);
-		if(*(ret+i)==NULL)
-		{
-			return NULL;
-		}
-	}
-	va_end(ap);
-	return ret;
+  if(n<=0||node==NULL)
+  {
+    return NULL;
+  }
+  struct node **ret=(struct node **)malloc(n*sizeof(struct node *));
+  if(ret==NULL)
+  {
+    return NULL;
+  }
+  *ret=node;
+  va_list ap;
+  va_start(ap,node);
+  for(size_t i=1;i<n;i++)
+  {
+    *(ret+i)=va_arg(ap,struct node *);
+    if(*(ret+i)==NULL)
+    {
+      return NULL;
+    }
+  }
+  va_end(ap);
+  return ret;
 }
 struct registered_function **bundle_registered_functions(size_t n,struct registered_function *function,...)
 {
-	if(n<=0||function==NULL)
-	{
-		return NULL;
-	}
-	struct registered_function **ret=(struct registered_function **)malloc(n*sizeof(struct registered_function *));
-	if(ret==NULL)
-	{
-		return NULL;
-	}
-	*ret=function;
-	va_list ap;
-	va_start(ap,function);
-	for(size_t i=1;i<n;i++)
-	{
-		*(ret+i)=va_arg(ap,struct registered_function *);
-		if(*(ret+i)==NULL)
-		{
-			return NULL;
-		}
-	}
-	va_end(ap);
-	return ret;
+  if(n<=0||function==NULL)
+  {
+    return NULL;
+  }
+  struct registered_function **ret=(struct registered_function **)malloc(n*sizeof(struct registered_function *));
+  if(ret==NULL)
+  {
+    return NULL;
+  }
+  *ret=function;
+  va_list ap;
+  va_start(ap,function);
+  for(size_t i=1;i<n;i++)
+  {
+    *(ret+i)=va_arg(ap,struct registered_function *);
+    if(*(ret+i)==NULL)
+    {
+      return NULL;
+    }
+  }
+  va_end(ap);
+  return ret;
 }
 struct screen **bundle_screen(size_t n,struct screen *screen,...)
 {
-	if(n<=0||screen==NULL)
-	{
-		return NULL;
-	}
-	struct screen **ret=(struct screen **)malloc(n*sizeof(struct screen *));
-	if(ret==NULL)
-	{
-		return NULL;
-	}
-	*ret=screen;
-	va_list ap;
-	va_start(ap,screen);
-	for(size_t i=1;i<n;i++)
-	{
-		*(ret+i)=va_arg(ap,struct screen *);
-		if(*(ret+i)==NULL)
-		{
-			return NULL;
-		}
-	}
-	va_end(ap);
-	return ret;
+  if(n<=0||screen==NULL)
+  {
+    return NULL;
+  }
+  struct screen **ret=(struct screen **)malloc(n*sizeof(struct screen *));
+  if(ret==NULL)
+  {
+    return NULL;
+  }
+  *ret=screen;
+  va_list ap;
+  va_start(ap,screen);
+  for(size_t i=1;i<n;i++)
+  {
+    *(ret+i)=va_arg(ap,struct screen *);
+    if(*(ret+i)==NULL)
+    {
+      return NULL;
+    }
+  }
+  va_end(ap);
+  return ret;
 } 
 struct node *makenode(struct screen **screen,struct node **node,size_t nscreens,size_t nnodes,size_t nopts,struct node_registry *reg)
 {
-	if(screen==NULL)
-	{
-		return NULL;
-	}
-	if(node==NULL&&nnodes!=0)
-	{
-		return NULL;
-	}
-	if(reg==NULL)
-	{
-		return NULL;
-	}
-	struct node *ret=(struct node *)malloc(nnodes*sizeof(struct node));
-	if(ret==NULL)
-	{
-		return NULL;
-	}
-	ret->screen=screen;
-	ret->node=node;
-	ret->nscreens=nscreens;
-	ret->nnodes=nnodes;
-	ret->nopts=nopts;
-	ret->node_registry=reg;
-	return ret;
+  if(screen==NULL)
+  {
+    return NULL;
+  }
+  if(node==NULL&&nnodes!=0)
+  {
+    return NULL;
+  }
+  if(reg==NULL)
+  {
+    return NULL;
+  }
+  struct node *ret=(struct node *)malloc(nnodes*sizeof(struct node));
+  if(ret==NULL)
+  {
+    return NULL;
+  }
+  ret->screen=screen;
+  ret->node=node;
+  ret->nscreens=nscreens;
+  ret->nnodes=nnodes;
+  ret->nopts=nopts;
+  ret->node_registry=reg;
+  return ret;
 }
 struct node_registry *makenodereg(char *opt_order,size_t n,registered_function **registered_function)
 {
-	struct node_registry *ret=(struct node_registry *)malloc(n*sizeof(struct node_registry));
-	if(ret==NULL)
-	{
-		return NULL;
-	}
-	ret->opt_order=opt_order;
-	ret->n=n;
-	ret->registered_funciton=registered_function;
-	return ret;
+  struct node_registry *ret=(struct node_registry *)malloc(n*sizeof(struct node_registry));
+  if(ret==NULL)
+  {
+    return NULL;
+  }
+  ret->opt_order=opt_order;
+  ret->n=n;
+  ret->registered_funciton=registered_function;
+  return ret;
 }
 struct screen *makescreen(size_t n,char *line,...)
 {
-	if(line==NULL)
-	{
-		return NULL;
-	}
-	struct screen *ret=(struct screen *)malloc(sizeof(struct screen));
-	if(ret==NULL)
-	{
-		return NULL;
-	}
-	char **arr=(char **)malloc(n*sizeof(char *));
-	if(arr==NULL)
-	{
-		return NULL;
-	}
-	char *line1=(char *)malloc((strlen(line)+1)*sizeof(char));
-	if(line1==NULL)
-	{
-		return NULL;
-	}
-	strcpy(line1,line);
-	*arr=line1;	
-	va_list ap;
-	va_start(ap,line);
-	for(size_t i=1;i<n;i++)
-	{
-		char *p=va_arg(ap,char *);
-		char *temp=(char *)malloc((1+strlen(p))*sizeof(char));
-		if(temp==NULL)
-		{
-			return NULL;
-		}
-		strcpy(temp,p);
-		*(arr+i)=temp;
-	}
-	va_end(ap);
-	ret->line=arr;
-	ret->n=n;
-	return ret;
+  if(line==NULL)
+  {
+    return NULL;
+  }
+  struct screen *ret=(struct screen *)malloc(sizeof(struct screen));
+  if(ret==NULL)
+  {
+    return NULL;
+  }
+  char **arr=(char **)malloc(n*sizeof(char *));
+  if(arr==NULL)
+  {
+    return NULL;
+  }
+  char *line1=(char *)malloc((strlen(line)+1)*sizeof(char));
+  if(line1==NULL)
+  {
+    return NULL;
+  }
+  strcpy(line1,line);
+  *arr=line1; 
+  va_list ap;
+  va_start(ap,line);
+  for(size_t i=1;i<n;i++)
+  {
+    char *p=va_arg(ap,char *);
+    char *temp=(char *)malloc((1+strlen(p))*sizeof(char));
+    if(temp==NULL)
+    {
+      return NULL;
+    }
+    strcpy(temp,p);
+    *(arr+i)=temp;
+  }
+  va_end(ap);
+  ret->line=arr;
+  ret->n=n;
+  return ret;
 }
 void node_depth_up(struct node **node,size_t index)
 {
-	*node=(*node)->node[index];
+  *node=(*node)->node[index];
 }
 char *order_opts(size_t nopts,char *bin_sem)
 {
-	if(nopts==0)
-	{
-		return NULL;
-	}
-	char *ret=(char *)malloc((1+nopts)*sizeof(char));
-	if(ret==NULL)
-	{
-		return NULL;
-	}
-	for(size_t i=0;i<nopts;i++)
-	{
-		*(ret+i)='0';
-	}
-	*(ret+nopts)='\0';
-	if(bin_sem==NULL)
-	{
-		return ret;
-	}
-	if(strlen(bin_sem)!=nopts)
-	{
-		free(ret);
-		return NULL;
-	}
-	for(size_t i=0;i<nopts;i++)
-	{
-		if(*(bin_sem+i)=='1')
-		{
-			*(ret+i)='1';
-		}
-		else if(*(bin_sem+i)!='0')
-		{
-			free(ret);
-			return NULL;
-		}
-	}
-	return ret;
+  if(nopts==0)
+  {
+    return NULL;
+  }
+  char *ret=(char *)malloc((1+nopts)*sizeof(char));
+  if(ret==NULL)
+  {
+    return NULL;
+  }
+  for(size_t i=0;i<nopts;i++)
+  {
+    *(ret+i)='0';
+  }
+  *(ret+nopts)='\0';
+  if(bin_sem==NULL)
+  {
+    return ret;
+  }
+  if(strlen(bin_sem)!=nopts)
+  {
+    free(ret);
+    return NULL;
+  }
+  for(size_t i=0;i<nopts;i++)
+  {
+    if(*(bin_sem+i)=='1')
+    {
+      *(ret+i)='1';
+    }
+    else if(*(bin_sem+i)!='0')
+    {
+      free(ret);
+      return NULL;
+    }
+  }
+  return ret;
 }
 struct registered_function *register_function(void(*function)(void))
 {
-	if(function==NULL)
-	{
-			return NULL;
-	}
-	registered_function *ret=(struct registered_function *)malloc(sizeof(struct registered_function));
-	if(ret==NULL)
-	{
-		return NULL;
-	}
-	ret->function=function;
-	return ret;
+  if(function==NULL)
+  {
+      return NULL;
+  }
+  registered_function *ret=(struct registered_function *)malloc(sizeof(struct registered_function));
+  if(ret==NULL)
+  {
+    return NULL;
+  }
+  ret->function=function;
+  return ret;
 }
 void testvoid(void)
 {
-	printf("testvoid\n");
+  printf("testvoid\n");
 }
 void testcalib (void)
 {
-	printf("testcalib\n");
+  printf("testcalib\n");
 }
 void testvolts(void)
 {
-	printf("testvolts\n");
+  printf("testvolts\n");
 }
 void testfc(void)
 {
-	printf("testfc\n");
+  printf("testfc\n");
 }
 void teststream(void)
 {
-	printf("teststream\n");
+  printf("teststream\n");
 }
 void teststop(void)
 {
-	printf("teststop\n");
+  printf("teststop\n");
 }
 void testc(void)
 {
-	printf("testc\n");
+  printf("testc\n");
 }
 void testf(void)
 {
-	printf("testf\n");
+  printf("testf\n");
 }
 void testk(void)
 {
-	printf("testk\n");
+  printf("testk\n");
 }
 void testuk(void)
 {
-	printf("testuk\n");
+  printf("testuk\n");
 }
+screen *menu0= makescreen(2,"******MENU******","1:TEMP  2:LIGHT ");
+screen *menu1= makescreen(2,"3:SERIAL CONFIG ","****************");
+screen *temp0=  makescreen(2,"******TEMP******","1:UNITS 2:CALIB ");
+screen *light0= makescreen(2,"******LIGHT*****","1:VOLTS 2:FC    ");
+screen *serial0=makescreen(2,"*****SERIAL*****","1:STREAM 2:STOP ");
+screen *units0=makescreen(2,"******UNITS*****","1:C     2:F     ");
+screen *units1=makescreen(2,"3:K     4:µK    ","****************");
+registered_function *regtestcalib=register_function(testcalib);
+registered_function *regtestvolts=register_function(testvolts);
+registered_function *regtestfc=register_function(testfc);
+registered_function *regteststream=register_function(teststream);
+registered_function *regteststop=register_function(teststop);
+registered_function *regtestc=register_function(testc);
+registered_function *regtestf=register_function(testf);
+registered_function *regtestk=register_function(testk);
+registered_function *regtestuk=register_function(testuk);
+node *units=makenode(bundle_screen(2,units0,units1),NULL,2,0,4,makenodereg(order_opts(4,"1111"),4,bundle_registered_functions(4,regtestc,regtestf,regtestk,regtestuk)));
+node *serial=makenode(&serial0,NULL,1,0,2,makenodereg(order_opts(2,"11"),2,bundle_registered_functions(2,regteststream,regteststop)));
+node *light=makenode(&light0,NULL,1,0,2,makenodereg(order_opts(2,"11"),2,bundle_registered_functions(2,regtestvolts,regtestfc)));
+node *temp=makenode(&temp0,bundle_node(1,units),1,1,2,makenodereg(order_opts(2,"01"),1,bundle_registered_functions(1,regtestcalib)));
+node *menu=makenode(bundle_screen(2,menu0,menu1),bundle_node(3,temp,light,serial),2,3,3,makenodereg(order_opts(3,NULL),0,NULL));
+node *root=menu;
